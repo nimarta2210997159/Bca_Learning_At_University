fb67c945e4f5afcd4d9bcb95fdfd4c4b6c02a06d
nimarta2210997159 <127485240+nimarta2210997159@users.noreply.github.com>
2023-04-27 17:25:04 +0530

    Update Assignment8.c

--- Assignment8.c
+++ Assignment8.c
@@ -2,7 +2,32 @@
 //input: Anitha
 //output: Aahint 
 
-int main()
-{
-  return 0;
+#include <stdio.h>
+#include <string.h>
+
+void selectionSort(char str[], int n);
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+    selectionSort(str, strlen(str));
+    printf("Sorted string: %s", str);
+    return 0;
+}
+
+void selectionSort(char str[], int n) {
+    int i, j, min_idx;
+    char temp;
+    for (i = 0; i < n-1; i++) {
+        min_idx = i;
+        for (j = i+1; j < n; j++) {
+            if (str[j] < str[min_idx]) {
+                min_idx = j;
+            }
+        }
+        temp = str[i];
+        str[i] = str[min_idx];
+        str[min_idx] = temp;
+    }
 }
